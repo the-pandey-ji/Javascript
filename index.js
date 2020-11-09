008078c1668493731c4a0e29fc569ff517042648
the-pandey-ji <sonu.pandey_cs18@gla.ac.in>
2020-11-09 23:17:34 +0530

    console cleared

--- index.js
+++ index.js
@@ -1,1 +1,6 @@
-console.log("hello sonu")
+console.log("hello sonu");
+console.log(4+65);
+console.log([25,54,65,24]);
+console.table({sonu:'Pandey',dharm:'Sharma'});
+console.warn('THis is warning');
+console.clear();
