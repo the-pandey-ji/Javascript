94a29229840bcf5a6e56d197c26af734ed493fbc
the-pandey-ji <sonu.pandey_cs18@gla.ac.in>
2020-11-09 23:19:37 +0530

    time end

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
+console.time('time taken');
 console.log("hello sonu");
 console.log(4+65);
 console.log([25,54,65,24]);
 console.table({sonu:'Pandey',dharm:'Sharma'});
 console.warn('THis is warning');
-console.clear();
+// console.clear();
+console.timeEnd('time taken');
