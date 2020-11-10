b2b74af5e2adef24833a4692289b2eabca76e4ff
the-pandey-ji <sonu.pandey_cs18@gla.ac.in>
2020-11-10 09:29:57 +0530

    variable.js created

---
+++ variables.js
@@ -0,0 +1,23 @@
+console.log('variables tutorial');
+
+var name = `So"n'u`;
+var channel;
+var marks = 99;
+
+console.log(name,channel,marks);
+
+var city = 'mathura';
+city = 'Delhi';
+console.log(city);
+
+const city1 = 'mathura';
+// city1 = 'Delhi';
+console.log(city1);
+
+{
+let city = 'mathura';
+city = 'agra';
+console.log(city);
+
+}
+console.log(city);
